37dff33e918c45edabac05dfdb8c179f412caf3c
Utkarsh Singh <utkarshsingh4345@gmail.com>
2023-11-22 22:32:23 +0530

    Time: 0 ms (100.00%) | Memory: 6.6 MB (94.56%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        
+        stack<char> s1;
+
+        for(char ch : s)
+        {
+            if(ch == '(' || ch == '{' || ch == '[')
+            {
+                s1.push(ch);
+            }
+
+            else
+            {
+                if(ch == ')')
+                {
+                    if(s1.empty() || s1.top() != '(') 
+                        return false;
+                }
+
+                if(ch == '}')
+                {
+                    if(s1.empty() || s1.top() != '{') 
+                        return false;
+                }
+
+                if(ch == ']')
+                {
+                    if(s1.empty() || s1.top() != '[') 
+                        return false;
+                }
+
+                s1.pop();
+            }
+
+        }
+
+        return s1.empty();
+    }
+};
