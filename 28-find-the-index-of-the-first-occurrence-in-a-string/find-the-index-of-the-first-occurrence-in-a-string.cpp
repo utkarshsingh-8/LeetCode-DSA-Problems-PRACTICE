5c0f6efe927681b615afd73a2dbed29558c1b08d
Utkarsh Singh <utkarshsingh4345@gmail.com>
2023-11-22 21:57:35 +0530

    Time: 3 ms (45.27%) | Memory: 6.7 MB (51.67%) - LeetSync

--- 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -3,10 +3,7 @@ public:
     int strStr(string haystack, string needle) {
 
         int x = haystack.size(), y = needle.size();
-
-        if(y == 0) return -1;
-        if(x == 1 && y == 1) return 0;
-
+          
         for(int i = 0; i <= x-y; i++)
         {
             for(int j = 0; j < y; j++)
