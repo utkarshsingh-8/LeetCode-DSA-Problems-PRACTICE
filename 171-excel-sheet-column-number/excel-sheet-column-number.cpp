bf9fd6a4462148d648e30cf8ed9f0b4a77b8fc33
Utkarsh Singh <utkarshsingh4345@gmail.com>
2023-11-24 20:20:19 +0530

    Time: 0 ms (100.00%) | Memory: 6.5 MB (35.95%) - LeetSync

--- 171-excel-sheet-column-number/excel-sheet-column-number.cpp
+++ 171-excel-sheet-column-number/excel-sheet-column-number.cpp
@@ -2,24 +2,13 @@ class Solution {
 public:
     int titleToNumber(string columnTitle) {
 
-        map<char,int> mp = {
-
-        {'A',1},{'B',2},{'C',3},{'D',4},
-        {'E',5},{'F',6},{'G',7},{'H',8},
-        {'I',9},{'J',10},{'K',11},{'L',12},
-        {'M',13},{'N',14},{'O',15},{'P',16},
-        {'Q',17},{'R',18},{'S',19},{'T',20},
-        {'U',21},{'V',22},{'W',23},{'X',24},
-        {'Y',25},{'Z',26}
-
-        };
-
         int n = columnTitle.size();
-        int ans = 0,j = 0;
+        int ans = 0, j = 0;
 
         for(int i = n - 1; i >= 0; i--)
         {
-            ans += pow(26,j++) * mp[columnTitle[i]];
+        
+            ans += pow(26,j++) * (columnTitle[i] - 'A' + 1);;
         }
 
         return ans;
