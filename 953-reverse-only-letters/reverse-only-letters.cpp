73200f0c85d8fc4b69ea7b675c121b14f35f9147
Utkarsh Singh <utkarshsingh4345@gmail.com>
2023-11-30 17:21:50 +0530

    Time: 0 ms (100.00%) | Memory: 6.4 MB (53.35%) - LeetSync

---
+++ 953-reverse-only-letters/reverse-only-letters.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    string reverseOnlyLetters(string s) {
+
+        int left = 0, right = s.size() - 1 ;
+
+        while(left < right)
+        {
+
+           if(!isalpha(s[left]))
+           {
+               left++;
+           }
+
+           else if(!isalpha(s[right]))
+           {
+               right--;
+           }
+
+           else
+           {
+               swap(s[left++],s[right--]);
+           }
+
+        }
+
+        return s;
+        
+    }
+};
