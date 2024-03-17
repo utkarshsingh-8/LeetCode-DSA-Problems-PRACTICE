9892202d52a4627c026fcdec5f5cb4f4ad38bbd2
Utkarsh Singh <utkarshsingh4345@gmail.com>
2024-03-17 11:18:12 +0530

    Time: 19 ms (23.55%) | Memory: 27.8 MB (6.12%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -2,23 +2,9 @@ class Solution {
 public:
     int majorityElement(vector<int>& nums) {
         
-        unordered_map<int,int> mp;
-        int size = nums.size();
-        int rem = size/2;
-
-        for(int i = 0; i < size; i++)
-        {
-            mp[nums[i]]++;
-        }
-
-        for(auto it = mp.begin(); it != mp.end(); it++)
-        {
-            if(it->second > rem)
-            {
-                return it->first;
-            }
-        }
-
-        return -1;
+       sort(nums.begin(),nums.end());
+       int n = nums.size();
+       return nums[n/2];
+       
     }
 };
